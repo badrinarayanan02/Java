0d4845a0fb33f9243ad63535cfb7ad5041ef07b1
badrinarayanan02 <badrinarayanan.s@msam.christuniversity.in>
2023-12-09 23:49:30 +0530

    Initial Commit

---
+++ Robber.java
@@ -0,0 +1,48 @@
+// Lab 4
+
+abstract class Robber{
+    
+    abstract void RobbingClass();
+
+    public void MachineLearning(){
+        System.out.println("I love Machine Learning");
+
+    }
+
+}
+
+    class JavaProfessionalRobber extends Robber{
+
+    @Override
+    void RobbingClass(){
+        System.out.println("MscAI&ML");
+
+    }
+
+    int RowHouses(int[] moneyInRowHouses){
+
+        return 0;
+        
+    }
+
+    int RoundHouses(int[] moneyInRoundHouses){
+
+        return 0;
+        
+    }
+
+    int SquareHouses(int[] moneyInSquareHouses){
+
+        return 0;
+        
+    }
+
+    int MultiHouses(int[] moneyInMultiHouses){
+
+        return 0;
+        
+    }
+        
+    }
+
+
